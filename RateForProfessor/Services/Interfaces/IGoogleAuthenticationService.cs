07f07602dcc82148c68389073977a3f4aad316f3
lumbardhgashi <lg52753@ubt-uni.net>
2023-07-04 13:29:54 +0200

    .

--- RateForProfessor/Services/Interfaces/IGoogleAuthenticationService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace RateForProfessor.Services.Interfaces
-{
-    public interface IGoogleAuthenticationService
-    {
-        public Task Login(string email);
-        public Task GoogleResponse();
-        public Task LogOut();
-    }
-}
